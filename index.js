0fba401b36004a1f21bc231c7ac040ac62f73fe1
rodgersochieng <rodger.ochieng@student.moringaschool.com>
2022-08-17 01:02:38 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+ let num1 =1;
+ let num2 = 62;
+ let multiply=(num1 * num2);
 
-// Write your code here
+ console.log(multiply);
+
+let num3= 22;
+let num4= 9;
+ let mod = (num3 % num4);
+ console.log(mod);
+
+
+let max =Math.max (7,16,19,20);
+console.log(max);
+
+let random = Math.floor(Math.random() *10)+1;
+console.log(random);
